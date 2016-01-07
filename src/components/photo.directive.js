0960ad0f6d3716f50d9827365be12b60b15b3682
Paul Crowder <paul.crowder@blackbaud.com>
2016-01-07 09:05:40 -0500

    Use bindToController on photo directive

--- src/components/photo.directive.js
+++ src/components/photo.directive.js
@@ -9,18 +9,25 @@
                 barkPhotoUrl: '=',
                 barkPhotoGravatarEmail: '='
             },
-            link: function (scope, el) {
+            bindToController: true,
+            controller: angular.noop,
+            controllerAs: 'barkPhoto',
+            link: function (scope, el, attr, barkPhoto) {
                 function setImageUrl(url) {
                     el.css('background-image', 'url(\'' + url + '\')');
                 }
 
-                scope.$watch('barkPhotoUrl', function (newValue) {
+                scope.$watch(function () {
+                    return barkPhoto.barkPhotoUrl;
+                }, function (newValue) {
                     if (newValue) {
                         setImageUrl(newValue.replace('http://', '//'));
                     }
                 });
 
-                scope.$watch('barkPhotoGravatarEmail', function (newValue) {
+                scope.$watch(function () {
+                    return barkPhoto.barkPhotoGravatarEmail;
+                }, function (newValue) {
                     if (newValue) {
                         setImageUrl(gravatarService.url(newValue, {default: 'mm'}));
                     }
