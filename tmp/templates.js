69a60f8be282308ab74d41dc22c0f37a184c33e6
Cameron Beacham <cameron.beacham@blackbaud.com>
2017-01-06 15:57:25 -0500

    fixing a bug in add template

--- tmp/templates.js
+++ tmp/templates.js
@@ -48,7 +48,7 @@ angular.module('barkbaud.templates', []).run(['$templateCache', function($templa
         '            <div class="form-group">\n' +
         '              <label class="control-label">Category:</label>\n' +
         '              <select class="form-control" ng-change="behaviorTraningAdd.checkLoadValues(behaviorTrainingAdd.behaviortraining.category.name, behaviorTrainingAdd.behaviortraining.category.type, behaviorTrainingAdd.behaviortraining.category.source)" ng-model="behaviorTrainingAdd.behaviortraining.category">\n' +
-        '               <option ng-repeat="categoryOption in behaviorTrainingAdd.categories" ng-bind="categoryOption.name" value="{{categoryOption}}"></option>\n' +
+        '               <option ng-repeat="categoryOption in behaviorTrainingAdd.categories" ng-bind="categoryOption.name" value="categoryOption"></option>\n' +
         '             </select>\n' +
         '            </div>\n' +
         '          </div>\n' +
