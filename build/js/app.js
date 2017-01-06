493a691d1351e24600857740cebaa924d8bdf11b
Cameron Beacham <cameron.beacham@blackbaud.com>
2017-01-06 11:08:34 -0500

    adding auto update to categories on source change

--- build/js/app.js
+++ build/js/app.js
@@ -1398,7 +1398,7 @@ angular.module('barkbaud.templates', []).run(['$templateCache', function($templa
         '          <div class="col-sm-6">\n' +
         '            <div class="form-group">\n' +
         '              <label class="control-label">Source:</label>\n' +
-        '              <select class="form-control" ng-model="behaviorTrainingAdd.behaviortraining.source">\n' +
+        '              <select class="form-control" ng-change="behaviorTrainingAdd.loadCategories(behaviorTrainingAdd.behaviortraining.source)" ng-model="behaviorTrainingAdd.behaviortraining.source">\n' +
         '                <option ng-repeat="sourceOption in behaviorTrainingAdd.sources" ng-bind="sourceOption" value="{{sourceOption}}"></option>\n' +
         '              </select>\n' +
         '            </div>\n' +
