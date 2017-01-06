9b56906221e2dd87c4d9cd8d259db6627288493e
Cameron Beacham <cameron.beacham@blackbaud.com>
2017-01-06 09:29:34 -0500

    fixing error in template

--- build/js/app.js
+++ build/js/app.js
@@ -1399,8 +1399,8 @@ angular.module('barkbaud.templates', []).run(['$templateCache', function($templa
         '          <div class="col-sm-6">\n' +
         '            <div class="form-group">\n' +
         '              <label class="control-label">Source:</label>\n' +
-        '              <select class="form-control" ng-model="behaviorTrainingAdd.behaviortraining.source">\n' +
-        '                <option ng-repeat="source in behaviorTrainingAdd.sources" ng-change="behaviorTrainingAdd.loadCategories(source)" ng-bind="source" value="{{source}}"></option>\n' +
+        '              <select class="form-control" ng-change="behaviorTrainingAdd.loadCategories(source)" ng-model="behaviorTrainingAdd.behaviortraining.source">\n' +
+        '                <option ng-repeat="source in behaviorTrainingAdd.sources" ng-bind="source" value="{{source}}"></option>\n' +
         '              </select>\n' +
         '            </div>\n' +
         '          </div>\n' +
