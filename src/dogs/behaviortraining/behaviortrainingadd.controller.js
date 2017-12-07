51ac77b84e734e7373e5e4b1380005be73ba1d9f
Christopher Rodgers <Chris.Rodgers@blackbaud.me>
2017-12-07 17:59:10 -0500

    Updating to skyux 1.19.1. Fixing behavior/training form

--- src/dogs/behaviortraining/behaviortrainingadd.controller.js
+++ src/dogs/behaviortraining/behaviortrainingadd.controller.js
@@ -23,7 +23,7 @@
             self.categoryValues = null; 
             self.behaviortraining.value = null;
 
-            if (self.behaviortraining.category.type === 'codetable') {
+            if (self.behaviortraining.category.type === 'CodeTable') {
                 bbData.load({
                     data: 'api/dogs/ratings/categories/values?categoryName=' + encodeURIComponent(self.behaviortraining.category.name)
                 }).then(function (result) {
